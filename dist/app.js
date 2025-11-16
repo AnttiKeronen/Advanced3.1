ad738dd9d3945504fc9ac0c8e8b4ea51c97b7858
AnttiKeronen <antti.keronen@student.lut.fi>
2025-11-16 18:49:28 +0200

    joo

--- dist/app.js
+++ dist/app.js
@@ -3,27 +3,16 @@ import path from "path";
 import { fileURLToPath } from "url";
 const app = express();
 const PORT = 3000;
-// Fix __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
-// ----------------------
-// 1. Hello world route
-// ----------------------
 app.get("/hello", (req, res) => {
     res.json({ msg: "Hello world!" });
 });
-// ----------------------
-// 2. ID echoing route
-// ----------------------
 app.get("/echo/:id", (req, res) => {
     res.json({ id: req.params.id });
 });
-// ----------------------
-// 3. POST sum route
-// ----------------------
 app.post("/sum", (req, res) => {
     const { numbers } = req.body;
     if (!Array.isArray(numbers))
@@ -37,13 +26,9 @@ app.post("/users", (req, res) => {
     users.push({ name, email });
     res.json({ msg: "User successfully added" });
 });
-// ----------------------
-// 5. GET all users
-// ----------------------
 app.get("/users", (req, res) => {
     res.status(201).json(users);
 });
-// Default route for CG
 app.get("/", (req, res) => {
     res.send("Server running");
 });
